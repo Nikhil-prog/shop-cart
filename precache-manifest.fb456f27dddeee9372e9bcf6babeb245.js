f58d3beca40dae9534049d139ace3646475bbbdd
Nikhil <nikhil070g@gmail.com>
2022-01-01 22:28:32 +0530

    Updates

---
+++ precache-manifest.fb456f27dddeee9372e9bcf6babeb245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bd43329a03431f0fd98",
+    "url": "/shop-cart/static/css/main.05230bb9.chunk.css"
+  },
+  {
+    "revision": "6bd43329a03431f0fd98",
+    "url": "/shop-cart/static/js/main.6bd43329.chunk.js"
+  },
+  {
+    "revision": "ec6103b3d0a317bc4649",
+    "url": "/shop-cart/static/js/1.ec6103b3.chunk.js"
+  },
+  {
+    "revision": "140db54b4dd141c41ec5",
+    "url": "/shop-cart/static/js/2.140db54b.chunk.js"
+  },
+  {
+    "revision": "53a177dd18d9c1689906",
+    "url": "/shop-cart/static/js/runtime~main.53a177dd.js"
+  },
+  {
+    "revision": "b32690e9d8cc794a9533db9141e0a36e",
+    "url": "/shop-cart/index.html"
+  }
+];
